9da243dd1c6be38e69090fceb7ac62c91749d628
Debid Magar <linkwithdm@gmail.com>
2021-07-07 12:22:36 +0545

    ready for production

--- app.js
+++ app.js
@@ -1,34 +1,42 @@
 // Imports
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const cors = require('cors');
+const morgan = require("morgan");
+const cors = require("cors");
 // const bodyParser = require('body-parser');
 const config = require("./config/config.json");
 const APIRoute = require("./apiRoute");
 require("./config/dbConfig");
 
-// Middleware 
+// Middleware
 app.use(morgan("tiny")); //for log
 //app.use(cors);
 
-// app.use(bodyParser);     
+// app.use(bodyParser);
 
 //For reading json and urlencoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 //API Routes
 const api = config.API_URL;
 app.use(`${api}/`, APIRoute);
 
-
 //Server connection
-app.listen(config.PORT, () => {
-    console.log(`Server is running at port : ${config.PORT}`);
-  });
+// app.listen(config.PORT, () => {
+//     console.log(`Server is running at port : ${config.PORT}`);
+//   });
 
+// //for pro
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || "0.0.0.0";
+app.listen(server_port, server_host, (err, done) => {
+  if (err) {
+    console.log("Error while listening port " + app.get("port") + " >> " + err);
+  } else {
+    console.log("Server is listening at port " + app.get("port"));
+  }
+});
 
 //For error handling
 app.use(function (err, req, res, next) {
--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,19 +1,36 @@
-const mongoose = require('mongoose');
-const config = require('./config.json');
+const mongoose = require("mongoose");
+const config = require("./config.json");
 
 // const conxn = `config.${DATABASE.DB_URL} / config.${DATABASE.DB_NAME}`;
-const conxn = config.url + '/' + config.dbname
-
+const conxn = config.url + "/" + config.dbname;
 
 //for dev and local database server
+// mongoose.connect(
+//   conxn,
+//   {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false,
+//   },
+//   function (err, done) {
+//     if (err) {
+//       console.log("Error connecting to db >>" + err);
+//     } else {
+//       console.log("db connection successs");
+//     }
+//   }
+// );
+
+//for remote
 mongoose.connect(
-    conxn,
-    { useNewUrlParser: true,useCreateIndex:true, useUnifiedTopology: true , useFindAndModify: false },
-    function (err, done) {  
-        if (err) {
-            console.log('Error connecting to db >>' + err)
-        } else {
-            console.log('db connection successs')
-        }
+  "mongodb+srv://debid:D4v!d123@cluster0.bumti.mongodb.net/eclass?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  function (err, done) {
+    if (err) {
+      console.log("Error connecting to db >>" + err);
+    } else {
+      console.log("db connection success");
     }
-)
+  }
+);
